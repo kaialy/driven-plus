d959c96b06767a1f4a5850ead4915cfb5220a4d2
jeimilycaialypc <jcaialy@gmail.com>
2023-03-26 16:46:21 -0300

    requisitos: login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import DataProvider from "./context/UserContext"
+import Login from "./components/authentication/Login"
+import SignUp from "./components/authentication/SignUp"
+import PlanInfo from "./components/onboard/subscription/PlanInfo"
+import Home from "./components/onboard/homepage/Home"
+import Subscription from "./components/onboard/subscription/Subscription"
 
-function App() {
+export default  function App() {
   return (
     <div >
       driven plus
     </div>
   );
 }
-
-export default App;
